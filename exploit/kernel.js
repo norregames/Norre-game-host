a74b097cc8a1f2a8d6c0bb1ae960a1ba198eb640
norregames <norrehakou@gmail.com>
2026-08-19 16:18:49 +0100

    Delete exploit directory

--- exploit/kernel.js
+++
@@ -1,4 +0,0 @@
-// Déclencheur Kernel Exploit 9.00 et injecteur de Payloads
-function injectPayload(payloadFile) {
-    console.log("Injection du payload: " + payloadFile);
-}
--- exploit/psfree.js
+++
@@ -1,5 +0,0 @@
-// Moteur d'exploitation WebKit PSFree sans USB
-function runExploit() {
-    console.log("PSFree Webkit Exploit initié...");
-    // Logique technique de débordement mémoire PSFree
-}
